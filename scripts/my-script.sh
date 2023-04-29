3c8ee15ffb5ceb5d2f6e2cdf0373844b8de320e6
k22063917 <k22063917@kcl.ac.uk>
2023-04-29 13:28:29 +0100

    my-script.sh

---
+++ scripts/my-script.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+# Logon to Open Stack
+
+# Make a directory that I would conduct my assignment
+mkdir K22063917_Adv_Bioinformatics_Assignment
+
+# Getting and installing Conda
+# Download Miniconda3 latest version on Open Stack
+curl -LO https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+
+#Then run this bash script to install miniconda3
+bash Miniconda3-latest-Linux-x86_64.sh
+
+# To reload our current shell session
+source ~/.bashrc
+
+# Create a new conda environment
+# to install new packages within the new-env
+# environment location: /home/ubuntu/miniconda3/envs/new-env
+conda create -n new-env
+
+#to enter the new-env
+conda activate new-env
+
+#Then install the packages required
+conda install trimmomatic
+conda install samtools
+conda install bwa
+conda install freebayes
+conda install picard
+conda install bedtools
+conda install fastqc
+
+
+#vcffilter already available within miniconda, no need to install
+#proceed
+
